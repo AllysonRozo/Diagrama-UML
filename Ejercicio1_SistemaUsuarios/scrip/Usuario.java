662fe078a4cb032ab3dd4180776a71f94094e754
AllysonRozo <allyson.villalba@correo.tdea.edu.co>
2025-11-15 08:49:37 -0500

    Add files via upload

---
+++ Ejercicio1_SistemaUsuarios/scrip/Usuario.java
@@ -0,0 +1,29 @@
+package Ejercicio1.script;
+
+public class Usuario {
+    private String nombre;
+    private int id;
+    private String password;
+
+    public Usuario(String nombre, int id, String password) {
+        this.nombre = nombre;
+        this.id = id;
+        this.password = password;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getPassword() {
+       return password;
+    }
+    public void leerNota() {
+        System.out.println("Leyendo nota...");
+    }
+    public void logear() {
+        System.out.println("Usuario " + nombre + " logeado.");
+    }
+
+}
